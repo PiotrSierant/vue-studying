0a17d4473757f48c76a15587de4cbec01af9bb37
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-07 14:28:07 +0100

    form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,59 @@
 <template>
   <div id="app">
-    <ComponentPlayerCharacter />
+    <ComponentTheAddPanelChamp 
+      @add="add"
+      :names="namesList"
+      />
+    <ComponentChampPanel 
+      v-for="champion in champions" 
+      :key="champion.name"
+      :name="champion.name"
+      :nick="champion.nick"
+      :hp="champion.hp"
+      :mana="champion.mana"
+      @remove="remove"
+      />
   </div>
 </template>
 
 <script>
-import ComponentPlayerCharacter from './components/ComponentPlayer.vue';
-
-
+import ComponentTheAddPanelChamp from './components/ComponentTheAddPanelChamp.vue';
+import ComponentChampPanel from './components/ComponentChampPanel.vue';
 export default {
     name: "App",
-    components: { ComponentPlayerCharacter }
+    components: { 
+      ComponentTheAddPanelChamp,
+      ComponentChampPanel,
+    },
+    data: () => ({
+      champions: [{
+        name: 'Me', 
+        nick: 'haha',
+        hp: 100,
+        mana: 100,
+      },
+      {
+        name: 'elo', 
+        nick: 'haha',
+        hp: 100,
+        mana: 100,
+      },
+      ]
+    }),
+    methods: {
+      remove(name) {
+          const index= this.champions.findIndex(v => v.name === name);
+          this.champions.splice(index, 1);
+      },
+      add(value){
+        this.champions.push(value)
+      }
+    },
+    computed: {
+      namesList() {
+        return this.champions.map(element => element.name)
+      }
+    }
 }
 </script>
 
---
+++ src/components/ComponentChampPanel.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            <section class="panel-title my-class">
+                <h4>Champion</h4>
+                <button class="btn btn-danger" type="button" @click="remove">Delete</button>
+            </section>
+        </div>
+        <div class="panel-body">
+            <div class="input-group margin-bottom">
+                <span class="input-group-addon">Hero Name</span>
+                <span class="input-group-addon">{{name}}</span>
+            </div>
+            <div class="input-group margin-bottom">
+                <span class="input-group-addon">Hero Nick</span>
+                <span class="input-group-addon">{{nick}}</span>
+            </div>
+        </div>
+        <div class="row margin-bottom">
+            <div class="col-md-6">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-danger progress-bar-striped" role="progressbar" style="width: 100%">
+                    {{hp}}/{{hp}} HP</div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="progress">
+                    <div class="progress-bar progress-bar-primary progress-bar-striped" role="progressbar" style="width: 100%">
+                    {{mana}}/{{mana}} MANA</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ComponentChampPanel',
+    methods: {
+        remove() {
+            this.$emit('remove', this.name)
+        }
+    },
+    props: {
+        name: {
+            type: String,
+            default() {
+                return ''
+            }
+        },
+        nick: {
+            type: String,
+            default() {
+                return ''
+            }
+        },
+        hp: {
+            type: [String, Number],
+            default() {
+                return 0
+            }
+        },
+        mana: {
+            type: [String, Number],
+            default() {
+                return 0
+            }
+        }
+    }
+}
+</script>
+<style scoped>
+    .my-class {
+        display: flex;
+        justify-content: space-between;
+    }
+    .margin-bottom {
+        margin-bottom: 1rem;
+    }
+</style>
