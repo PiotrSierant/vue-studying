9cd79107791b17ac49ff5385d26ca0d5778957d3
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-08 09:14:59 +0100

    transition, animation in vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,24 @@
 <template>
   <div>
-    
+    <header>
+      <h1>Animation, transition</h1>
+      <button @click="display = !display" class="btn" :class="[display ? 'btn-success':'btn-danger']">Check animation</button>
+    </header>
+    <section class="animatedSection" >
+      <transition name="fade" mode="out-in">
+        <h2 v-if="display" class="alert alert-info" key="1">Witaj!</h2>
+        <h2 v-else class="alert alert-danger" key="2">Cześć!</h2>
+      </transition>
+    </section>       
   </div>
 </template>
 
 <script>
 export default {
     name: "App",
-    
+    data: () => ({
+      display: false,
+    }),
 }
 </script>
 
@@ -15,5 +26,23 @@ export default {
 body {
   margin: 0;
   padding: 0;
+}
+header,section {
+  padding: 1rem;
+}
+.animatedSection {
+  width: 100%;
+  height: 10vh;
+  border-bottom: .1rem dashed red;
+  border-top: 1px dashed red;
+}
+.fade-enter-active {
+  transition: .5s;
+}
+.fade-leave-active {
+  transition: .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
