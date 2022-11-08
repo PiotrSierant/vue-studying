1d4d4a4b46be0deae4de59e750ffd5fb32672d5a
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-08 09:18:43 +0100

    refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
 <template>
   <div>
-    <header>
-      <h1>Animation, transition</h1>
-      <button @click="display = !display" class="btn" :class="[display ? 'btn-success':'btn-danger']">Check animation</button>
-    </header>
-    <section class="animatedSection" >
-      <transition name="fade" mode="out-in">
-        <h2 v-if="display" class="alert alert-info" key="1">Witaj!</h2>
-        <h2 v-else class="alert alert-danger" key="2">Cześć!</h2>
-      </transition>
-    </section>       
+       <ComponentAnimation />
   </div>
 </template>
 
 <script>
+import ComponentAnimation from './components/ComponentAnimation.vue';
 export default {
     name: "App",
-    data: () => ({
-      display: false,
-    }),
+    components: {
+      ComponentAnimation
+    },
 }
 </script>
 
@@ -29,20 +21,5 @@ body {
 }
 header,section {
   padding: 1rem;
-}
-.animatedSection {
-  width: 100%;
-  height: 10vh;
-  border-bottom: .1rem dashed red;
-  border-top: 1px dashed red;
-}
-.fade-enter-active {
-  transition: .5s;
-}
-.fade-leave-active {
-  transition: .3s;
-}
-.fade-enter, .fade-leave-to {
-  opacity: 0;
 }
 </style>
---
+++ src/components/ComponentAnimation.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <header>
+        <h1>Animation, transition</h1>
+        <button @click="display = !display" class="btn" :class="[display ? 'btn-success':'btn-danger']">Check animation</button>
+        </header>
+        <section class="animatedSection" >
+        <transition name="fade" mode="out-in">
+            <h2 v-if="display" class="alert alert-info" key="1">Witaj!</h2>
+            <h2 v-else class="alert alert-danger" key="2">Cześć!</h2>
+        </transition>
+        </section>    
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentAnimation',
+    data: () => ({
+      display: false,
+    }),
+};
+</script>
+
+<style>
+.animatedSection {
+  width: 100%;
+  height: 10vh;
+  border-bottom: .1rem dashed red;
+  border-top: 1px dashed red;
+}
+.fade-enter-active {
+  transition: .5s;
+}
+.fade-leave-active {
+  transition: .3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
