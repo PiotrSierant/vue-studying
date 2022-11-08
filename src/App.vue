413cdf4996b364309d5e429e1b62ff2b97031984
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-08 13:25:14 +0100

    nav, routes

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/contact">Contact</router-link>
-    </nav>
+    <ComponentNavigation />
     <router-view/>
   </div>
 </template>
-
+<script>
+import ComponentNavigation from '@/components/ComponentNavigation.vue';
+export default {
+  name: 'app',
+  components: {
+    ComponentNavigation,
+  },
+}
+</script>
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-  margin: 0 1rem;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/ComponentNavigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav>
+        <ul class="nav nav-pills ">
+            <router-link to="/" active-class="active" exact role="presentation" tag="li"><a>Home</a></router-link>
+            <router-link to="/about" active-class="active" role="presentation" tag="li"><a>About</a></router-link>
+            <router-link to="/contact" active-class="active" role="presentation" tag="li"><a>Contact</a></router-link>
+        </ul>
+    </nav>
+</template>
+<script>
+export default {
+    name: 'ComponentNavigation',
+}
+</script>
+<style scoped>
+nav {
+    border: none;
+    width: 100%;
+    box-shadow: 0 0 10px 1px black;
+    padding: 2rem;
+    border-bottom: .3rem solid darkgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+ul {
+    max-width: 1366px;
+    width: 100%;
+}
+</style>
