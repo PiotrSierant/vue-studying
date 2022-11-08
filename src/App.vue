49c0b95649d323523f2f7b0d1b97461481e15e2f
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-08 11:58:57 +0100

    animated component

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,48 @@
 <template>
   <div>
-       <ComponentAnimation />
+    <header>    
+      <h1>Animated nav-tabs</h1>
+      <ul class="nav nav-tabs">
+          <li :class="{ active: cmp === 'ComponentHome'}" @click="cmp = 'ComponentHome'"><a href="#">Home</a></li>
+          <li :class="{ active: cmp === 'ComponentSecret'}" @click="cmp = 'ComponentSecret'"><a href="#">Secret</a></li>
+          <li :class="{ active: cmp === 'ComponentPublic'}" @click="cmp = 'ComponentPublic'"><a href="#">Public</a></li>
+      </ul>
+      <transition name="slide" mode="out-in">
+        <KeepAlive include="ComponentPublic">
+          <component :is="cmp" />
+        </KeepAlive>
+      </transition>
+       <!-- <ComponentAnimation />
        <ComponentAnimation2 />
        <ComponentAnimation3 />
-       <ComponentAnimation4 />
+       <ComponentAnimation4 /> -->
+    </header>
   </div>
 </template>
 
 <script>
-import ComponentAnimation from './components/ComponentAnimation.vue';
-import ComponentAnimation2 from './components/ComponentAnimation2.vue';
-import ComponentAnimation3 from './components/ComponentAnimation3.vue';
-import ComponentAnimation4 from './components/ComponentAnimation4.vue';
+// import ComponentAnimation from './components/ComponentAnimation.vue';
+// import ComponentAnimation2 from './components/ComponentAnimation2.vue';
+// import ComponentAnimation3 from './components/ComponentAnimation3.vue';
+// import ComponentAnimation4 from './components/ComponentAnimation4.vue';
+import ComponentHome from './components/ComponentHome.vue';
+import ComponentSecret from './components/ComponentSecret.vue';
+import ComponentPublic from './components/ComponentPublic.vue';
 
 export default {
     name: "App",
     components: {
-      ComponentAnimation,
-      ComponentAnimation2,
-      ComponentAnimation3,
-      ComponentAnimation4,
-
+    //   ComponentAnimation,
+    //   ComponentAnimation2,
+    //   ComponentAnimation3,
+    //   ComponentAnimation4,
+         ComponentHome,
+         ComponentSecret,
+         ComponentPublic,
     },
+    data: () => ({
+      cmp: 'ComponentHome',
+    })
 }
 </script>
 
@@ -40,4 +61,18 @@ header, section {
 .btn {
   padding: 1rem 2rem;
 }
+.slide-enter-active, .slide-leave-active {
+        transition: .5s;
+    }
+    .slice-enter, .slide-leave-to {
+        transition: .5s;
+        transform: translateX(100%);
+    }
+    .slide-move {
+        transition: transform 1s;
+    }
+    .slide-leave-active {
+        position: absolute;
+        width: 100%;
+    }
 </style>
---
+++ src/components/ComponentHome.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h1>Component Home</h1>
+        <button disabled=true class="btn btn-primary">Learn more</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ComponentHome',
+
+    data() {
+        return {
+            
+        };
+    },
+};
+</script>
+
+<style>
+
+</style>
