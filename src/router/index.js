77e70c3bc6b7ac516664c295e48be4cb079f9642
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-09 11:16:38 +0100

    prepare project

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import ComponentContactDetails from '@/components/ComponentContactDetails.vue';
-import ComponentContactEdit from '@/components/ComponentContactEdit.vue';
+// import ComponentContactDetails from '@/components/ComponentContactDetails.vue';
+// import ComponentContactEdit from '@/components/ComponentContactEdit.vue';
 import ErrorView from '@/views/ErrorView.vue';
 Vue.use(VueRouter)
 
@@ -12,30 +12,30 @@ const routes = [
     name: 'home',
     component: HomeView
   },
-  {
-    path: '/about',
-    name: 'about',
-    component: () => import('../views/AboutView.vue')
-  },
-  {
-    path: '/contact',
-    name: 'contact',
-    component: () => import('../views/ContactView.vue'),
-    children: [
-      { path: ':name', component: ComponentContactDetails, props: true, name: 'contact-name' },
-      { path: ':name/edit', component: ComponentContactEdit,
-        beforeEnter: (to, from, next) => {
-          const password = prompt('Password: ')
-          if(password === 'abc'){
-            next()
-          }
-          else {
-            next(false)
-          }
-        }
-      }
-    ]
-  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   component: () => import('../views/AboutView.vue')
+  // },
+  // {
+  //   path: '/contact',
+  //   name: 'contact',
+  //   component: () => import('../views/ContactView.vue'),
+  //   children: [
+  //     { path: ':name', component: ComponentContactDetails, props: true, name: 'contact-name' },
+  //     { path: ':name/edit', component: ComponentContactEdit,
+  //       beforeEnter: (to, from, next) => {
+  //         const password = prompt('Password: ')
+  //         if(password === 'abc'){
+  //           next()
+  //         }
+  //         else {
+  //           next(false)
+  //         }
+  //       }
+  //     }
+  //   ]
+  // },
   {
     path: '/error',
     name: 'error',
