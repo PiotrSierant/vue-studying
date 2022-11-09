f672c896bde4041f2d0fa7aa0da2008f1c99abff
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-09 12:51:42 +0100

    hooki Bind, Inserted and Unbind

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <template>
   <div id="app">
-    <h1 v-padding>Directive</h1>
+    <section class="height">
+      <h1 v-padding v-if="!visible">Directive</h1>
+    </section>
+    <input 
+      type="checkbox" 
+      v-model="visible" 
+      @click="visible = !visible"
+    /> <span v-if="visible">Pokaż</span>
+    <span v-else>Ukryj</span>
   </div>
 </template>
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      visible: false,
+    }
+  }
 }
 </script>
 <style>
@@ -13,6 +26,9 @@ body, *, *::after, *::before {
   padding: 0;
   margin: 0;
   background-color: rgb(228, 228, 228);
-  color: #111
+  color: #111;
+}
+.height {
+  min-height: 10vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,29 @@
 import Vue from 'vue'
 import App from './App.vue'
 Vue.config.productionTip = false
+
+const reaction = () => {
+  alert('Klikniety')
+}
+
 Vue.directive('padding', {
+  bind(element) {
+    // bind uzywamy do pojedynczego elementu, nie mamy dostępu do rodzica
+    element.style.textAlign = 'center';
+    element.style.fontSize = '4rem';
+
+    element.addEventListener('click', reaction);
+  }, 
+  
   inserted(element) {
-    element.style.padding = '1rem';
+    // dzięki inserted mamy dostęp do dziecka i do rodzica
+    element.parentNode.style.padding = '1rem';
+    element.parentNode.style.backgroundColor = 'black';
+  }, 
+
+  unbind(element) {
+    // usuwa zdarzenia przed usunieciem elementu z dom
+    element.removeEventListener('click', reaction);
   }
 })
 new Vue({
