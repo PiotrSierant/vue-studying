cdf95db28012e1f27a4e8a8d240f3c7656b9bc18
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-10 10:31:36 +0100

    Start chapter02

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,11 @@
 <template>
   <div id="app">
-    <b-container>
-      <b-alert v-font-size="size" show variant="primary" v-shadow>
-        Primary Alert
-      </b-alert>
-      Change size <input type="number" v-model="size">
-      <hr>
-      <b-alert v-my-on:click="click" show variant="danger" v-shadow="'rgba(255, 0, 0, .5)'">
-        Click
-      </b-alert>
-      <b-alert v-my-on:click="click" show variant="danger" v-shadow="color">
-        <input type="text" v-model="color">
-      </b-alert>
-      <hr>
-      <ProjectZadanie01 />
-    </b-container>
+
   </div>
 </template>
 <script>
-import ProjectZadanie01 from './components/Zadanie01.vue';
 export default {
   name: 'app',
-  components: {
-    ProjectZadanie01,
-  },
-  data() {
-    return {
-      size: 2,
-      color: 'rgba(25, 50, 250, .5)',
-    }
-  },
-  methods: {
-    click() {
-      console.log(this.color);
-    }
-  },
-  directives: { // dyrektywy lokalnie
-    //   shadow: {
-    //     bind(element, binding) {
-    //       const color = binding.value ? binding.value : 'rgba(255, 255, 0, .5)';
-    //       element.style.boxShadow = `10px 10px 36px 0 ${color}`;
-
-    //     if (binding.modifiers['red']) {
-    //       element.style.boxShadow = '10px 10px 36px 0 rgba(255, 0, 0, .5)';
-    //     }
-    //   },
-    //    update(element, binding) {
-    //     const color = binding.value ? binding.value : 'rgba(255, 255, 0, .5)';
-    //     element.style.boxShadow = `10px 10px 36px 0 ${color}`;
-    //   }
-    // },
-    shadow(element, binding) {
-      const color = binding.value ? binding.value : 'rgba(255, 255, 0, .5)';
-      element.style.boxShadow = `10px 10px 36px 0 ${color}`;
-
-    },
-    // zamiast używać dwóch na raz możemy zrobić funkcje z shadow jak wyżej 
-  }
 }
 </script>
 <style>
@@ -68,9 +17,5 @@ body,
   margin: 0;
   background-color: rgb(228, 228, 228);
   color: #111;
-}
-
-.height {
-  min-height: 10vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,62 +3,9 @@ import App from './App.vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
-Vue.config.productionTip = false
-Vue.use(BootstrapVue)
-// const reaction = () => {
-//   alert('Klikniety')
-// }
+Vue.config.productionTip = false;
+Vue.use(BootstrapVue);
 
-Vue.directive('my-on', {
-  bind(element, binding) {
-    element.addEventListener(binding.arg, binding.value);
-  },
-  update(element, binding) {
-    element.removeEventListener(binding.arg, binding.oldValue);
-    element.addEventListener(binding.arg, binding.value);
-  },
-  unbind(element, binding) {
-    element.removeEventListener(binding.arg, binding.oldValue);
-  }
-})
-
-Vue.directive('font-size', {
-  update(element, binding) {
-    console.log(`Name of directive is: ${binding.name}`)
-    element.style.fontSize = `${binding.value}rem`;
-
-  },
-  bind(element, binding) {
-    element.style.textAlign = 'center';
-    element.style.fontSize = `${binding.value}rem`;
-  },
-})
-
-Vue.directive('padding', {
-  bind(element) {
-    // bind uzywamy do pojedynczego elementu, nie mamy dostępu do rodzica
-    element.style.textAlign = 'center';
-    element.style.fontSize = '4rem';
-    // element.addEventListener('click', reaction);
-  },
-
-  inserted(element) {
-    // dzięki inserted mamy dostęp do dziecka i do rodzica
-    element.parentNode.style.padding = '1rem';
-    element.parentNode.style.backgroundColor = 'black';
-  },
-
-  // unbind(element) {
-  // usuwa zdarzenia przed usunieciem elementu z dom
-  // element.removeEventListener('click', reaction);
-  // },
-  update(element) {
-    console.log('update', element.innerHTML);
-  },
-  componentUpdated(element) {
-    console.log('ComponentUpdate', element.innerHTML);
-  }
-})
 new Vue({
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
