5aa0b112ee046a3ae4363b3b3173a74b3e075c69
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-14 09:27:37 +0100

    [Vuelidate] przygotowanie plikow do pracy z Vuelidate

--- src/App.vue
+++ src/App.vue
@@ -1,73 +1,16 @@
 <template>
-  <div class="my-app">
-    <b-container>
-      <transition name="fade" mode="out-in">
-        <div key="1" v-if="loading" class="d-flex justify-content-center my-5">
-          <b-spinner variant="primary" />
-        </div>
-
-        <b-row v-else>
-          <b-col sm="12" v-for="(quote, index) in quotes" :key="index">
-            <b-card :title="quote.title" tag="article" class="my-2">
-              <div sm="12" v-html="quote.content">
-              </div>
-
-              <b-button variant="primary" size="sm" @click='getNewOne(index)'>Losuj kolejny</b-button>
-
-            </b-card>
-          </b-col>
-        </b-row>
-
-      </transition>
-    </b-container>
+  <div>
+    <h1>Vuelidate</h1>
   </div>
 </template>
-
 <script>
 export default {
-  data() {
-    return {
-      loading: true,
-      quotes: []
-    };
-  },
-  methods: {
-    async getNewOne(index) {
-      this.loading = true;
-      const url = `http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&timestamp=${new Date().getTime()}`;
-      try {
-        let { data } = await this.axios(url);
-        if (data === undefined) {
-          this.quotes.push(data.shift());
-        } else {
-          this.$set(this.quotes, index, data.shift());
-        }
-      }
-      catch (error) {
-        console.log(error.message);
-      }
-    }
-  },
-  async created() {
-    await this.axios.all([this.getNewOne(), this.getNewOne(), this.getNewOne()])
-    this.loading = false;
+  name: 'app',
+  data: () => {
+
   }
 }
 </script>
+<style scoped>
 
-<style>
-.my-app {
-  padding: 50px 0;
-}
-
-/* Fade transition */
-.fade-enter-active,
-.fade-leave-active {
-  transition: opacity .5s;
-}
-
-.fade-enter,
-.fade-leave-to {
-  opacity: 0;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import App from './App.vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vuelidate from 'vuelidate';
 
 Vue.use(BootstrapVue);
 Vue.use(VueAxios, axios);
+Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
