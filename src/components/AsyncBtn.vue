80241fd719581bb69f521f81cef93e02cb70e0e4
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-16 11:22:13 +0100

    [Test] testowanie asynchronicznych zachowan

---
+++ src/components/AsyncBtn.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <button @click="getData">Wczytaj dane</button>
+        <span>{{ bestPlayer }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectAsyncBtn',
+
+    data() {
+        return {
+            bestPlayer: null
+        };
+    },
+
+    methods: {
+        async getData() {
+            let { data } = await this.$http.get('http://api.icndb.com/jokes/random');
+            this.bestPlayer = data.player
+        }
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
