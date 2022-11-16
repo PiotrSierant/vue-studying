858b1dc38114fc64a20bb74cd9fd1827b748eef3
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-16 13:45:08 +0100

    [Test] stubbing

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <HelloWorld />
+        <UsersList />
+    </div>
+</template>
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+import UsersList from '@/components/UsersList.vue';
+
+export default {
+    name: 'ProjectMain',
+    components: {
+        HelloWorld,
+        UsersList,
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/UsersList.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>Users list...</div>
+</template>
+
+<script>
+export default {
+    name: 'ProjectUsersList',
+    created() {
+        console.log('Something async happends here!');
+    }
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
