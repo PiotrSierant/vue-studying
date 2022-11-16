901bb4305e3bf90bdb57299d78690d03b826d981
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-16 12:18:41 +0100

    bug fix

--- tests/unit/ComputedTDD.spec.js
+++ tests/unit/ComputedTDD.spec.js
@@ -4,12 +4,10 @@ import ComputedTDD from "@/components/ComputedTDD.vue";
 
 describe('ComputedTDD.vue', () => {
     it('properly renders odd numbers', () => {
-        const wrapper = shallowMount(ComputedTDD, {
-            propsData: {
-                odd: true,
-            }
-        })
-        expect(wrapper.text()).to.be.equal('1, 3, 5, 7, 9');
+        const localThis = { odd: true };
+        const computedValue = ComputedTDD.computed.nums.call(localThis);
+
+        expect(computedValue).to.be.equal('1, 3, 5, 7, 9');
     })
     it('properly renders even numbers', () => {
         const localThis = { odd: false };
