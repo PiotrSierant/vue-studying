ec1bfea9fb10cf19657ab07e49435c2138447529
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-16 09:22:10 +0100

    [Login/Register]

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
-import Vue from 'vue';
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-import App from './App.vue';
-import BootstrapVue from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
-import Vuelidate from 'vuelidate';
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import Vuelidate from 'vuelidate'
+Vue.use(Vuelidate)
 
 Vue.use(BootstrapVue);
-Vue.use(VueAxios, axios);
-Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
+Vue.use(VueAxios, axios)
 
 new Vue({
-  render: h => h(App),
+  router,
+  store,
+  render: h => h(App)
 }).$mount('#app')
