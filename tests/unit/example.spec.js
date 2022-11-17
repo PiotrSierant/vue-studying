e5fcdb67a49264b9e140ac856ace431afe1c3865
PiotrSierant <piotr.sierant96@gmail.com>
2022-11-17 08:36:32 +0100

    finish chapter tests

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -6,12 +6,12 @@ should();
 
 describe("HelloWorld.vue", () => {
     it("renders props.msg when passed", () => {
-        // const msg = "new message";
-        const wrapper = shallowMount(HelloWorld)
-        // const wrapper = shallowMount(HelloWorld, {
-        //     propsData: { msg },
-        // });
-        // expect(wrapper.text()).to.include(msg);
+        const msg = "new message";
+        // const wrapper = shallowMount(HelloWorld)
+        const wrapper = shallowMount(HelloWorld, {
+            propsData: { msg },
+        });
+        expect(wrapper.text()).to.include(msg);
         const x = wrapper.text();
         x.should.include('Hello World!');
     });
